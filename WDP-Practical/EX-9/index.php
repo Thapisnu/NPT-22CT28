7fa803e87f86b5ec05ddb512f9d2b0a9e2df423e
Thapisnu <tthapisnu@gmail.com>
2024-02-17 21:20:16 +0530

    22CT28-WDP-EX9

---
+++ WDP-Practical/EX-9/index.php
@@ -0,0 +1,15 @@
+<?php
+
+    #Date Functions
+    echo "<h4>Current Year is ".date("Y")."</h4>";
+    echo "<h4>Current Month is ".date("m")."</h4>";
+    echo "<h4>Current Day is ".date("d")."</h4>";
+    echo "<h4>Current Date is ".date("d/m/Y")."</h4>";
+
+    #Time Functions
+    echo "<h4>Current Minute is ".date("i")."</h4>";
+    echo "<h4>Current Hour[12] is ".date("h")."</h4>";
+    echo "<h4>Current Hour[24] is ".date("H")."</h4>";
+    echo "<h4>Current Time is ".date("h:i:s:a")."</h4>";
+    
+?>
